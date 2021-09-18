4f6802f9f0ac9f5e2a909e4ab2cfbf71648a1b1d
falkTX <falktx@falktx.com>
2021-09-18 10:31:38 +0100

    Setup documentation steps for new files
    Signed-off-by: falkTX <falktx@falktx.com>

--- generate-lv2-html-docs.sh
+++ generate-lv2-html-docs.sh
@@ -20,10 +20,12 @@ if [ ! -f documentation/style.css ]; then
   git clone git://github.com/KXStudio/LV2-Extensions --depth 1 -b gh-pages documentation
 fi
 
-cp kx-external-ui.lv2/* documentation/extui/
-cp kx-programs.lv2/*    documentation/progs/
-cp kx-rtmempool.lv2/*   documentation/rtmpl/
+cp kx-control-input-port-change-request.lv2/* documentation/cipcr/
+cp kx-external-ui.lv2/*                       documentation/extui/
+cp kx-programs.lv2/*                          documentation/progs/
+cp kx-rtmempool.lv2/*                         documentation/rtmpl/
 
-lv2specgen.py $(pwd)/kx-external-ui.lv2/manifest.ttl /usr/share/lv2specgen/ ../style.css $(pwd)/documentation/extui/index.html $(pwd)/documentation/extui "" -i -p extui
-lv2specgen.py $(pwd)/kx-programs.lv2/manifest.ttl    /usr/share/lv2specgen/ ../style.css $(pwd)/documentation/progs/index.html $(pwd)/documentation/progs "" -i -p progs
-lv2specgen.py $(pwd)/kx-rtmempool.lv2/manifest.ttl   /usr/share/lv2specgen/ ../style.css $(pwd)/documentation/rtmpl/index.html $(pwd)/documentation/rtmpl "" -i -p rtmpl
+lv2specgen.py $(pwd)/kx-control-input-port-change-request.lv2/manifest.ttl /usr/share/lv2specgen/ ../style.css $(pwd)/documentation/cipcr/index.html $(pwd)/documentation/cipcr "" -i -p cipcr
+lv2specgen.py $(pwd)/kx-external-ui.lv2/manifest.ttl                       /usr/share/lv2specgen/ ../style.css $(pwd)/documentation/extui/index.html $(pwd)/documentation/extui "" -i -p extui
+lv2specgen.py $(pwd)/kx-programs.lv2/manifest.ttl                          /usr/share/lv2specgen/ ../style.css $(pwd)/documentation/progs/index.html $(pwd)/documentation/progs "" -i -p progs
+lv2specgen.py $(pwd)/kx-rtmempool.lv2/manifest.ttl                         /usr/share/lv2specgen/ ../style.css $(pwd)/documentation/rtmpl/index.html $(pwd)/documentation/rtmpl "" -i -p rtmpl
